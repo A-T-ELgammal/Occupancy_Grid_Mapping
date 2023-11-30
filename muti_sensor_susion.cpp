ffd221ab3726560f7ee4f761ddd71455aafd9b56
Ahmed Elgammal <a.t.elgammal@gmail.com>
2023-11-30 04:16:37 +0200

    pass by reference

--- muti_sensor_susion.cpp
+++ muti_sensor_susion.cpp
@@ -6,28 +6,23 @@ using namespace std;
 const int rows = 2;
 const int cols = 2;
 
-vector<vector<double>> sensorFusion(double m1[][rows], double m2[][rows])
+vector<vector<double>> sensor_fusion(const vector << vector > double >> &m1, const vector << vector > double >> &m2)
 {
-    //*#############TODO: Code the Sensor Fusion Function############*//
-    // Fuse the measurments of the two maps and print the resulting
-    // map in a matrix form:
-    // a  b
-    // c  d
     vector<vector<double>> fusion_map(rows, vector<double>(cols, 0));
-    for (int row = 0; row < cols; row++)
-        for (int col = 0; col < rows; col++)
-            fusion_map[row][col] = 1 - ((1 - m1[row][col]) * (1 - m2[row][col]));
+    for (int r = 0; r < rows; r++)
+        for (int c = 0; c < cols; c++)
+            fusion_map[r][c] = 1 - (1 - (m1[r][c]) * (1 - m2[r][c]));
     return fusion_map;
 }
 
 int main()
 {
 
-    double m1[cols][rows] = {{0.9, 0.6}, {0.1, 0.5}};
-    double m2[cols][rows] = {{0.3, 0.4}, {0.4, 0.3}};
-    vector<vector<double>> fusion_map = sensorFusion(m1, m2);
-    for (int row = 0; row < cols; row++)
-        for (int col = 0; col < rows; col++)
-            cout << fusion_map[row][col] << " ";
+    vector<vector<double>> m1{{0.9, 0.6}, {0.1, 0.5}};
+    vector<vector<double>>{{0.3, 0.4}, {0.4, 0.3}};
+    vector<vector<double>> fusion_map = sensor_fusion(m1, m2);
+    for (int r = 0; r < rows; r++)
+        for (int c = 0; c < cols; c++)
+            cout << fusion_map[r][c];
     return 0;
 }
